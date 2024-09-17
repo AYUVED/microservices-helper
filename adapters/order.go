26de67f5de37bbbdd3221085c3b9fb8eaff0b6a3
nileshdt <nileshdt@gmail.com>
2024-09-16 22:26:20 -0400

    m

---
+++ adapters/order.go
@@ -0,0 +1,47 @@
+package order
+
+import (
+	"context"
+
+	"github.com/ayuved/microservices-proto/golang/order"
+	"github.com/ayuved/microservices-helper/domain"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Adapter struct {
+	order order.OrderClient
+}
+
+func NewAdapter(orderServiceUrl string) (*Adapter, error) {
+	var opts []grpc.DialOption
+	opts = append(opts,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithUnaryInterceptor(otelgrpc.UnaryClientInterceptor()),
+	)
+	conn, err := grpc.Dial(orderServiceUrl, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	client := order.NewOrderClient(conn)
+	//defer conn.Close()
+	return &Adapter{order: client}, nil
+}
+
+func (a *Adapter) Order(ctx context.Context, o *domain.Order) error {
+	var items []*order.OrderItem
+	for _, item := range o.OrderItems {
+		items = append(items, &order.OrderItem{
+			ProductCode: item.ProductCode,
+			UnitPrice:   item.UnitPrice,
+			Quantity:    item.Quantity,
+		})
+	}
+	_, err := a.order.Create(ctx, &order.CreateOrderRequest{
+		UserId:     o.CustomerID,
+		OrderItems: items,
+	})
+	return err
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module github.com/ayuved/microservices-helper
 
 go 1.22.3
+
+require (
+	github.com/ayuved/microservices-proto/golang/order v1.0.2
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.55.0
+	google.golang.org/grpc v1.66.2
+)
+
+require (
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel v1.30.0 // indirect
+	go.opentelemetry.io/otel/metric v1.30.0 // indirect
+	go.opentelemetry.io/otel/trace v1.30.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
